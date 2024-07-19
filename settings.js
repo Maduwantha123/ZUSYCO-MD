e2624e1d81f281c74d00b3a987ce783b9e94b22b
Maduwantha123 <151824417+Maduwantha123@users.noreply.github.com>
2024-07-19 21:20:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,15 +16,15 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
+  OWNER_NUMBER: "94773076130",
   SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
-  BOT_NAME : "ＺＵＳＹＣＯ",
+  OWNER_NAME: "『 𝙷𝙰𝚂𝙷𝙰𝙽 𝙺𝙰𝚅𝙸𝚂𝙷𝙺𝙰 』",
+  BOT_NAME : "🇰🇷 𝐌𝐑 𝐇𝐀𝐒𝐇𝐈𝐘𝐀 𝐎𝐅𝐂 🇰🇷",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
-    ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
-    WELCOME: 'https://i.ibb.co/PzhpBV7/WELCOME-IMAGE-LOWQ.jpg',
-    GOODBYE: 'https://i.ibb.co/t8hKpcM/GOODBYE-IMAGE-LOWQ.jpg'
+    ALIVE: 'https://telegra.ph/file/042bb401b6d3eb6854849.jpg',
+    WELCOME: 'https://telegra.ph/file/cbb77643c92769ab9a181.jpg',
+    GOODBYE: 'https://telegra.ph/file/5e758a8e0df09554a81d3.jpg'
   },
   STOP_SEND : {
         WELCOME_MESSAGE : false,
@@ -35,12 +35,12 @@ module.exports = {
   WORK_TYPE : "public",
   WORK_MODE : "onlygroups",
   WORK_MODE_MESSAGE_SEND : false,
-  CAPTION : '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ᴢᴜꜱʏᴄᴏ ᴍᴅ ʙᴏᴛ',
-  STICKER_PACK : '[ ZUSYCO ]',
+  CAPTION : '©🇰🇷 𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃 𝐁𝐘 𝐌𝐑 𝐇𝐀𝐒𝐇𝐈𝐘𝐀 🇰🇷',
+  STICKER_PACK : '[ 𝙼𝚁 𝙷𝙰𝚂𝙷𝙸𝚈𝙰 𝙱𝙾𝚃 𝙶𝚁𝙾𝚄𝙿 ]',
   BAD_WORDS : 'fuck,pussy',
   ANTI_BAD : false,
   ANTI_LINKS : 'chat.whatsapp.com',
-  ANTI_LINK : false,
+  ANTI_LINK : true,
   ANTI_LINK_KICK : false,
   MAX_SIZE : 100,
   MOROCCO_NUMBERS : {
@@ -54,8 +54,8 @@ module.exports = {
   },
   BOT_OFFLINE : false,
   INBOX_BLOCK : false,
-  AUTO_REACT : false,
-  OWNER_REACT : '🤵‍♂️',
+  AUTO_REACT : true,
+  OWNER_REACT : '👨‍💻',
   AUTO_BGM : false,
   BGM : {
     "chaio" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-caio.mp3",
